49a51719d80a4a635da096acdf3b9902d1df2758
Arthur Junior <arthurjr1906@gmail.com>
2023-08-26 19:08:50 -0300

    feat: add stacks section on main page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,10 @@
-import { About } from "@/components";
+import { About, Stacks } from "@/components";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col h-full min-h-screen">
       <About />
+      <Stacks />
     </main>
   )
 }
