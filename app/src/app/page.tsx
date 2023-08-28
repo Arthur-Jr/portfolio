33079e5b100bf8f09ed067977acaad527c81b13c
Arthur Junior <arthurjr1906@gmail.com>
2023-08-27 23:47:21 -0300

    feat: Add email form section on main page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,11 @@
-import { About, Stacks } from "@/components";
+import { About, ContactForm, Stacks } from "@/components";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col h-full min-h-screen">
       <About />
       <Stacks />
+      <ContactForm />
     </main>
   )
 }
