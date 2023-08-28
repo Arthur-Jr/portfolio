98c927dbb5e95b21420cb45cbf77b02681e30540
Arthur Junior <arthurjr1906@gmail.com>
2023-08-27 23:47:06 -0300

    feat: Create Email form component

---
+++ app/src/components/ContactForm.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { FormEvent, useState } from "react";
+
+export default function ContactForm() {
+  const formInput = ['title', 'email'];
+  const [formData, setFormData] = useState({ title: '', email: '', subject: '' });
+  const [formMsg, setFormMsg] = useState('');
+
+  const validateEmail = (email: string) => {
+    const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return EMAIL_REGEX.test(email);
+  }
+
+  const handleFormInputChange = ({ name, value }: { name: string, value: string }) => {
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const SEVEN_SECONDS = 7000;
+  
+    if (validateEmail(formData.email)) {
+      setFormMsg('Email enviado com sucesso!');
+      setFormData({ title: '', email: '', subject: '' });
+    } else {
+      setFormMsg('Email inválido!');
+    }
+
+    setTimeout(() => {
+      setFormMsg('');
+    }, SEVEN_SECONDS)
+  }
+
+  return (
+    <section className="w-[100%] h-[900px] mt-[-60px] flex items-center flex-col">
+      <h1 className="text-[1.5rem] sm:text-[1.7rem] italic font-semibold mb-10 scroll-mt-20" id="contact">
+        Contato
+      </h1>
+
+      <form
+        className=" flex flex-col items-center justify-around w-[90%] md:w-[50%] h-[600px] py-3 bg-primary-color rounded-2xl shadow-md"
+        onSubmit={(e) => handleFormSubmit(e) }
+      >
+        {formInput.map((field) => (
+          <label
+          key={field}
+          htmlFor={`form-${field}`}
+          className="flex flex-col items-center justify-between w-[80%] h-20 font-semibold italic"
+          >
+            {field.toUpperCase()}
+            <input
+              className="w-[100%] p-3 rounded-xl"
+              type="text"
+              id={`form-${field}`}
+              placeholder={field.toUpperCase()}
+              name={field}
+              value={formData[field as keyof typeof formData]}
+              onChange={(e) => handleFormInputChange(e.target)}
+              required
+            />
+          </label>
+        ))}
+
+        <label
+          htmlFor="form-subject"
+          className="flex flex-col items-center justify-between w-[80%] font-semibold italic"
+        >
+          SUBJECT
+          <textarea
+            className="resize-none w-full h-[250px] p-3 rounded-xl"
+            name="subject"
+            id="form-subject"
+            placeholder="SUBJECT"
+            value={formData.subject}
+            onChange={(e) => handleFormInputChange(e.target)}
+            required
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="w-[50%] p-3 rounded-full bg-white font-semibold text-lg italic hover:scale-110"
+        >
+          Submit
+        </button>
+
+        { formMsg.length > 0 &&
+          <span
+            className="absolute p-3 rounded-lg bg-gray-500 text-white font-bold text-lg italic"
+          >
+            {formMsg}
+          </span>
+        }
+      </form>
+    </section>
+  )
+}
--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import About from "./About";
 import Stacks from "./Stacks";
+import ContactForm from "./ContactForm";
 
-export { Header, About, Stacks };
+export { Header, About, Stacks, ContactForm };
