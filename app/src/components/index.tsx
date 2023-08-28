e40453b30e77a53d2978e51d5d94ef9eea852861
Arthur Junior <arthurjr1906@gmail.com>
2023-08-28 01:22:26 -0300

    feat: Create socials sections

--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -2,5 +2,6 @@ import Header from "./Header";
 import About from "./About";
 import Stacks from "./Stacks";
 import ContactForm from "./ContactForm";
+import Socials from "./Socials";
 
-export { Header, About, Stacks, ContactForm };
+export { Header, About, Stacks, ContactForm, Socials };
