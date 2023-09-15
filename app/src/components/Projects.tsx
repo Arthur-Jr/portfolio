a4f255d1cb818b51736c9347b851e3061b061210
Arthur Junior <arthurjr1906@gmail.com>
2023-09-15 01:39:15 -0300

    feat: Implement grid on projects cards

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -1,30 +1,37 @@
 'use client';
 
 import Link from "next/link";
-import { useState } from "react";
+import Image from "next/image";
 
 import './projects.css';
 import { projects } from "@/constants/projects";
+import upArrow from "../../public/up-arrow-icon.svg";
 
 export default function Projects() {
-  const [projectId, setProjectId] = useState(0);
-
   return (
-    <section className="mt-[-4rem] w-[85%] flex flex-col items-center mb-14">
-      <h1 className="text-[1.5rem] sm:text-[1.7rem] mb-6 italic font-semibold scroll-mt-[4.5rem]" id="projects">
+    <section className="mt-[-4rem] w-[93%] md:w-[85%] flex flex-col items-center mb-14">
+      <h1 className="text-[1.5rem] sm:text-[1.7rem] mb-6 italic font-semibold scroll-mt-[5.5rem]" id="projects">
         Projetos
       </h1>
 
-      <div className="w-full h-full projects-div-grid mb-10">
+      <div className="w-full h-full mb-10 projects-div-grid">
         {projects.map((project) => (
-          <div key={project.id} className={`item-${project.id} h-full bg-primary-color rounded-xl`}>
-            test
+          <div key={project.id} className={`item-${project.id} project-card`}>
+            <div className="info-div">
+              <h2 className="project-name">{project.name}</h2>
+              <p className="project-text">{project.text}</p>
+              <div className="links-div">
+                <Link href={project.siteLink} target="_blank">Ver Site ➜</Link>
+                <Link href={project.gihubLink} target="_blank">Código ➜</Link>
+              </div>
+              <Image src={upArrow} alt="up arrow icon" width={19} className="info-div-arrow" />
+            </div>
           </div>
         ))}
       </div>
 
-      <span className=" mb-5 text-sm italic text-center px-2 mt-12 md:mt-2">
-        No meu <Link className="font-bold underline-offset-2 underline" href="https://github.com/Arthur-Jr">Github</Link> pode ser encontrado mais projetos!
+      <span className=" mb-5 text-sm italic text-center px-2 mt-6">
+        No meu <Link className="font-bold underline-offset-2 underline" href="https://github.com/Arthur-Jr" target="_blank">Github</Link> pode ser encontrado mais projetos!
       </span>
       <span className="border-t-2 border-slate-300 w-[85%]"></span>
     </section>
