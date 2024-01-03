8713dcd2456f20ce87d4a098ac959e445a0cbcf8
Arthur Junior <arthurjr1906@gmail.com>
2024-01-03 20:48:20 -0300

    feat: Add projects on projects section

--- app/src/constants/projects.ts
+++ app/src/constants/projects.ts
@@ -1,36 +1,37 @@
 import testImg from '../../public/projectImg.png';
+import recipeImg from '../../public/projects/recipes-img.png';
 
 export const projects = [
   {
     id: 0,
-    name: 'Gestor de Gasto',
-    text: 'Foi desenvolvido um jogo de perguntas e respostas baseado no jogo Trivia (tipo um show do milhão americano rs) utilizando React e Redux, desenvolvendo em grupo suas funcionalidades de acordo com as demandas definidas em um quadro Kanban, teremos uma aplicação onde a pessoa usuária poderá.',
-    gihubLink: 'https://github.com/Arthur-Jr/wallet',
-    siteLink: 'https://www.google.com',
+    name: 'User Hub',
+    text: 'Projeto Full Stack desenvolvido com next.js e node.js/mongoDB. Nesse projeto desenvolvi um user hub onde é possivel criar contas e administra-la, com uma conta criada nesse user hub é possivel usar essa mesma conta em outros projetos que criei. Como esse site consome uma API que está hospedada em um cloud free leva pelo menos 3 minutos para iniciar o back-end.',
+    gihubLink: 'https://github.com/Arthur-Jr/user-hub?tab=readme-ov-file',
+    siteLink: 'https://user-hub-kappa.vercel.app',
     img: testImg,
   },
   {
     id: 1,
-    name: 'Puzzle Game',
-    text: 'Foi desenvolvido um jogo de perguntas e respostas baseado no jogo Trivia (tipo um show do milhão americano rs) utilizando React e Redux, desenvolvendo em grupo suas funcionalidades de acordo com as demandas definidas em um quadro Kanban, teremos uma aplicação onde a pessoa usuária poderá:',
-    gihubLink: 'https://github.com/Arthur-Jr/wallet',
-    siteLink: 'https://www.google.com',
-    img: testImg,
+    name: 'Recipes',
+    text: 'Projeto desenvolvido em next.js com foco em Front End, onde consome uma API de receitas de terceiros. O foco da projeto é a busca de receitas por nome do prato, ingredientes e categoria, com um design ituitivo e responsivo para também poder ser consumido em dispositivos mobile.',
+    gihubLink: 'https://github.com/Arthur-Jr/recipe',
+    siteLink: 'https://main.dqgery3y364u2.amplifyapp.com',
+    img: recipeImg,
   },
   {
     id: 2,
-    name: 'Planet finder',
-    text: 'Neste projeto foi desenvolvido uma lista com filtros de planetas do universo de Star Wars, ao utilizar essa aplicação um usuário deverá ser capaz de filtrar/pesquisar planetas.',
-    gihubLink: 'https://github.com/Arthur-Jr/wallet',
-    siteLink: 'https://www.google.com',
+    name: 'Spotify Clone',
+    text: 'Projeto desenvolvido em next.js e tailwind. Foi desenvolvido um clone da inreface web de 2023 do spotify, com foco em copiar um design e refaze-lo com HTML e CSS.',
+    gihubLink: 'https://github.com/Arthur-Jr/spotify-cln',
+    siteLink: 'https://spotify-cln-iota.vercel.app',
     img: testImg,
   },
   {
     id: 3,
-    name: 'Recipes',
-    text: 'Foi desenvolvido um app de receitas, utilizando o que há de mais moderno dentro do ecossistema React: Hooks! Nela será possível ver, buscar, filtrar, favoritar e acompanhar o processo de preparação de receitas e drinks! A base de dados serão 2 APIs distintas, uma para comidas e outra para bebidas. O layout tem como foco dispositivos móveis, então todos os protótipos vão estar desenvolvidos em telas menores.',
-    gihubLink: 'https://github.com/Arthur-Jr/wallet',
-    siteLink: 'https://www.google.com',
+    name: 'TV Shows Schedule',
+    text: 'Projeto Full Stack desenvolvido com next.js e next.js API Route. Nesse projeto desenvolvi um calendario de programas de tv, onde é possivel adicionar um programa que esteja atualmente indo ao ar. Esse projeto consome a mesma API que o user hub para lidar com as contas, logo é nescessario esperar os 3 minutos para acessar o back end, mas é possivel usar esse projeto sem uma conta.',
+    gihubLink: 'https://github.com/Arthur-Jr/schedule',
+    siteLink: 'https://schedule-arthur-jr.vercel.app',
     img: testImg,
   }
 ];
